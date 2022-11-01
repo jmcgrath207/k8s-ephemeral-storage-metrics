0e286973f0d16631fa24f512aa3e9f129667da07
John McGrath <john.mcgrath207@gmail.com>
2022-11-01 01:02:38 -0500

    working on pod monitoring service.

--- main.go
+++ main.go
@@ -5,9 +5,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -19,11 +17,11 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"time"
 )
 
 var inCluster string
 var clientset *kubernetes.Clientset
-var reg *prometheus.Registry
 var currentNode string
 
 func getEnv(key, fallback string) string {
@@ -78,46 +76,45 @@ func getK8sClient() {
 
 func getMetrics() {
 
+	opsQueued := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "ephemeral_storage_pod_usage",
+		Help: "Used to expose Ephemeral Storage metrics for pod ",
+	},
+		[]string{
+			// name of pod for Ephemeral Storage
+			"pod_name",
+			// Name of Node where pod is placed.
+			"node_name",
+		},
+	)
+
+	prometheus.MustRegister(opsQueued)
+
 	log.Debug().Msg(fmt.Sprintf("getMetrics has been invoked"))
 	currentNode = getEnv("CURRENT_NODE_NAME", "")
-	content, err := clientset.RESTClient().Get().AbsPath(fmt.Sprintf("/api/v1/nodes/%s/proxy/stats/summary", currentNode)).DoRaw(context.Background())
-	if err != nil {
-		log.Error().Msg(fmt.Sprintf("ErrorBadRequst : %s\n", err.Error()))
-		os.Exit(1)
+	for {
+		content, err := clientset.RESTClient().Get().AbsPath(fmt.Sprintf("/api/v1/nodes/%s/proxy/stats/summary", currentNode)).DoRaw(context.Background())
+		if err != nil {
+			log.Error().Msg(fmt.Sprintf("ErrorBadRequst : %s\n", err.Error()))
+			os.Exit(1)
+		}
+		log.Debug().Msg(fmt.Sprintf("Fetched proxy stats from node : %s", currentNode))
+		var raw map[string]interface{}
+		_ = json.Unmarshal(content, &raw)
+
+		nodeName := raw["node"].(map[string]interface{})["nodeName"].(string)
+		for _, element := range raw["pods"].([]interface{}) {
+
+			podName := element.(map[string]interface{})["podRef"].(map[string]interface{})["name"].(string)
+			usedBytes := element.(map[string]interface{})["ephemeral-storage"].(map[string]interface{})["usedBytes"].(float64)
+
+			opsQueued.With(prometheus.Labels{"pod_name": podName, "node_name": nodeName}).Set(usedBytes)
+
+			log.Debug().Msg(fmt.Sprintf("pod %s on %s with usedBytes: %s", podName, nodeName, usedBytes))
+		}
+
+		time.Sleep(15 * time.Second)
 	}
-	log.Debug().Msg(fmt.Sprintf("Fetched proxy stats from node : %s", currentNode))
-	var raw map[string]interface{}
-	_ = json.Unmarshal(content, &raw)
-
-	nodeName := raw["node"].(map[string]interface{})["nodeName"].(string)
-
-	for _, element := range raw["pods"].([]interface{}) {
-
-		pod_name := element.(map[string]interface{})["podRef"].(map[string]interface{})["name"].(string)
-
-		opsQueued := promauto.With(reg).NewGauge(prometheus.GaugeOpts{
-			Name:        "ephemeral_storage_pod_usage",
-			Help:        "Used to expose Ephemeral Storage metrics for pod ",
-			ConstLabels: prometheus.Labels{"pod_name": pod_name, "node_name": nodeName},
-		})
-
-		// ERROR
-		// 2022/10/25 04:22:23 http: panic serving 127.0.0.1:47698: duplicate metrics collector registration attempted
-		// TODO: https://github.com/prometheus/client_golang/issues/716
-		prometheus.MustRegister(opsQueued)
-
-		usedBytes := element.(map[string]interface{})["ephemeral-storage"].(map[string]interface{})["usedBytes"].(float64)
-		opsQueued.Set(usedBytes)
-
-	}
-}
-
-func prometheusMiddleware(next http.Handler) http.Handler {
-	log.Debug().Msg("Invoked prometheusMiddleware")
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		getMetrics()
-		next.ServeHTTP(w, r)
-	})
 }
 
 type LineInfoHook struct{}
@@ -145,13 +142,10 @@ func main() {
 	flag.Parse()
 	setLogger()
 	getK8sClient()
-	reg = prometheus.NewRegistry()
-	r := mux.NewRouter()
-	r.Use(prometheusMiddleware)
-	r.Path("/metrics").Handler(promhttp.Handler())
+	go getMetrics()
 	port := getEnv("METRICS_PORT", "9100")
-	srv := &http.Server{Addr: fmt.Sprintf("localhost:%v", port), Handler: r}
-	err := srv.ListenAndServe()
+	http.Handle("/metrics", promhttp.Handler())
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 	if err != nil {
 		log.Error().Msg(fmt.Sprintf("Listener Falied : %s\n", err.Error()))
 		panic(err.Error())
