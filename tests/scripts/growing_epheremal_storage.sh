d0c0d8ef21fe4cfb5165ec6ddef0f8503e1f4676
John Mcgrath <john.mcgrath207@gmail.com>
2024-01-29 00:45:43 -0600

    feat: empty dir volumes

--- tests/scripts/growing_epheremal_storage.sh
+++ tests/scripts/growing_epheremal_storage.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 while true; do
-  dd if=/dev/zero of="grow_storage$(( RANDOM*1000^999)).txt" bs=1KB count=1
+  current_size=$(du /cache -s | grep -oP '\d+')
+  if [[ "$current_size" -gt 12000 ]]; then
+    # Max Size Reach. Kill Container
+    exit 1
+  fi
+  dd if=/dev/zero of="grow_storage$((RANDOM * 1000 ^ 999)).txt" bs=4KB count=1
+  dd if=/dev/zero of="/cache/grow_storage$((RANDOM * 1000 ^ 999)).txt" bs=4KB count=1
+  dd if=/dev/zero of="/cachez/grow_storage$((RANDOM * 1000 ^ 999)).txt" bs=4KB count=1
   sleep 1
 done
--- tests/scripts/shrinking_epheremal_storage.sh
+++ tests/scripts/shrinking_epheremal_storage.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+# Grow a storage path to 12MB each with 4Kb files to match ext4 the smallest possible block size.
+# https://www.kernel.org/doc/html/latest/filesystems/ext4/overview.html#blocks
 count=0
 while [ $count -lt 1000 ]; do
-  dd if=/dev/zero of="grow_storage$(( RANDOM*1000^999)).txt" bs=1KB count=1
+  dd if=/dev/zero of="shrink_storage$((RANDOM * 1000 ^ 999)).txt" bs=4KB count=1
+  dd if=/dev/zero of="/cache/shrink_storage$((RANDOM * 1000 ^ 999)).txt" bs=4KB count=1
+  dd if=/dev/zero of="/cachez/shrink_storage$((RANDOM * 1000 ^ 999)).txt" bs=4KB count=1
   count=$((count + 1))
 done
 
-ls grow_storage* | xargs -I % sh -c '{ rm %; sleep 1; }'
+# Non Blocking
+(
+  ls /cache/shrink_storage* | xargs -I % sh -c '{ rm %; sleep 1; }' || true
+) &
+(
+  ls /cachez/shrink_storage* | xargs -I % sh -c '{ rm %; sleep 1; }' || true
+) &
+
+# Blocking code
+ls shrink_storage* | xargs -I % sh -c '{ rm %; sleep 1; }'
 
 echo "Finished Shrinking file. Restart Pod"
-
-sleep infinity
