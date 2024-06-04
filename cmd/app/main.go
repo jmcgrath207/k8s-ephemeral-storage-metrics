1838346a972fea0ef679637e8f6c204f1a52f483
kimiya Sharifi <38203408+kimiyasharifi@users.noreply.github.com>
2024-06-04 10:40:18 +0330

    Expose metrics for namespaces with specific labels

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+  "os"
+	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -11,6 +13,9 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog/log"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"net/http"
 	"strconv"
 	"time"
@@ -21,7 +26,29 @@ var (
 	sampleIntervalMill int64
 	Node               node.Node
 	Pod                pod.Collector
+	clientset *kubernetes.Clientset
 )
+
+func getMonitoredNamespaces(clientset *kubernetes.Clientset) ([]string, error) {
+	labelSelector := os.Getenv("EPHEMERAL_STORAGE_LABEL")
+
+	if labelSelector == "" {
+		labelSelector = "ephemeral-storage-monitoring=enabled"
+	}
+
+	namespaceList, err := clientset.CoreV1().Namespaces().List(context.TODO(), v1.ListOptions{
+		LabelSelector: labelSelector,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var namespaces []string
+	for _, ns := range namespaceList.Items {
+		namespaces = append(namespaces, ns.Name)
+	}
+	return namespaces, nil
+}
 
 type ephemeralStorageMetrics struct {
 	Node struct {
@@ -42,7 +69,11 @@ type ephemeralStorageMetrics struct {
 	}
 }
 
-func setMetrics(nodeName string) {
+func setMetrics(clientset *kubernetes.Clientset, nodeName string, monitoredNamespaces map[string]bool) {
+	if clientset == nil {
+		log.Error().Msg("Kubernetes clientset is nil")
+		return
+	}
 
 	var data ephemeralStorageMetrics
 
@@ -55,7 +86,11 @@ func setMetrics(nodeName string) {
 	}
 
 	log.Debug().Msg(fmt.Sprintf("Fetched proxy stats from node : %s", nodeName))
-	_ = json.Unmarshal(content, &data)
+	err = json.Unmarshal(content, &data)
+	if err != nil {
+		log.Error().Msgf("Failed to unmarshal content: %v", err)
+		return
+	}
 
 	for _, p := range data.Pods {
 		podName := p.PodRef.Name
@@ -67,6 +102,13 @@ func setMetrics(nodeName string) {
 			log.Warn().Msg(fmt.Sprintf("pod %s/%s on %s has no metrics on its ephemeral storage usage", podName, podNamespace, nodeName))
 			continue
 		}
+
+
+		if !monitoredNamespaces[podNamespace] {
+			log.Debug().Msg(fmt.Sprintf("Skipping pod %s/%s as it does not meet label requirements", podName, podNamespace))
+			continue
+		}
+
 		Node.SetMetrics(nodeName, availableBytes, capacityBytes)
 		Pod.SetMetrics(podName, podNamespace, nodeName, usedBytes, availableBytes, capacityBytes, p.Volumes)
 	}
@@ -81,13 +123,23 @@ func setMetrics(nodeName string) {
 
 }
 
-func getMetrics() {
+func getMetrics(clientset *kubernetes.Clientset) {
 
 	Node.WaitGroup.Wait()
 	Pod.WaitGroup.Wait()
 
 	p, _ := ants.NewPoolWithFunc(Node.MaxNodeQueryConcurrency, func(node interface{}) {
-		setMetrics(node.(string))
+		monitoredNamespacesList, err := getMonitoredNamespaces(clientset)
+		if err != nil {
+			log.Error().Msgf("Failed to fetch monitored namespaces: %v", err)
+			return
+		}
+
+		monitoredNamespaces := make(map[string]bool)
+		for _, ns := range monitoredNamespacesList {
+			monitoredNamespaces[ns] = true
+		}
+		setMetrics(clientset, node.(string), monitoredNamespaces)
 	}, ants.WithExpiryDuration(time.Duration(sampleInterval)*time.Second))
 
 	defer p.Release()
@@ -107,7 +159,6 @@ func main() {
 	flag.Parse()
 	port := dev.GetEnv("METRICS_PORT", "9100")
 
-	// Shared Vars
 	pprofEnabled, _ := strconv.ParseBool(dev.GetEnv("PPROF", "false"))
 	sampleInterval, _ = strconv.ParseInt(dev.GetEnv("SCRAPE_INTERVAL", "15"), 10, 64)
 	sampleIntervalMill = sampleInterval * 1000
@@ -117,15 +168,62 @@ func main() {
 	Node = node.NewCollector(sampleInterval)
 	Pod = pod.NewCollector(sampleInterval)
 
+	var err error
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		log.Error().Msgf("Failed to create in-cluster config: %v", err)
+		return
+	}
+
+	config.QPS = 20.0
+	config.Burst = 40
+	clientset, err = kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Error().Msgf("Failed to create Kubernetes clientset: %v", err)
+		return
+	}
+
 	if pprofEnabled {
 		go dev.EnablePprof()
 	}
 	go Node.Get()
 	go Node.Watch()
-	go getMetrics()
+
+	go func() {
+		Node.WaitGroup.Wait()
+		Pod.WaitGroup.Wait()
+
+		p, _ := ants.NewPoolWithFunc(Node.MaxNodeQueryConcurrency, func(node interface{}) {
+			monitoredNamespacesList, err := getMonitoredNamespaces(clientset)
+			if err != nil {
+				log.Error().Msgf("Failed to fetch monitored namespaces: %v", err)
+				return
+			}
+			monitoredNamespaces := make(map[string]bool)
+			for _, ns := range monitoredNamespacesList {
+				monitoredNamespaces[ns] = true
+			}
+			setMetrics(clientset, node.(string), monitoredNamespaces)
+		}, ants.WithExpiryDuration(time.Duration(sampleInterval)*time.Second))
+
+		defer p.Release()
+
+		for {
+			nodeSlice := Node.Set.ToSlice()
+
+			for _, node := range nodeSlice {
+				_ = p.Invoke(node)
+			}
+
+			time.Sleep(time.Duration(sampleInterval) * time.Second)
+		}
+	}()
+
+	go getMetrics(clientset)
+
 	http.Handle("/metrics", promhttp.Handler())
 	log.Info().Msg(fmt.Sprintf("Starting server listening on :%s", port))
-	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	err = http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 	if err != nil {
 		log.Error().Msg(fmt.Sprintf("Listener Failed : %s\n", err.Error()))
 		panic(err.Error())
